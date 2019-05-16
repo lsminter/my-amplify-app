693900f11c79248c58f3d974e6cfb3aff1f3a12d
lucas <lsminteris@gmail.com>
2019-05-16 13:50:27 -0700

    graphql api and amplify cli
    Created a new GraphQL API using the Amplify CLI and use the Amplify GraphQL API. All the changes outside of the app.js file were created/changed by those two commands.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,44 @@
-
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { Auth } from 'aws-amplify'
+import { withAuthenticator } from 'aws-amplify-react'
+import { API, graphqlOperation } from 'aws-amplify'
+
+const ListTodos = `
+  query {
+    listTodos {
+      items {
+        id name description completed
+      }
+    }
+  }
+`
 
 class App extends Component {
-  state = { username: '', password: '', email: '', phone_number: '', authenticationCode: '', step: 0 }
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-  signUp = async () => {
-    const { username, password, email, phone_number } = this.state
-    try {
-      await Auth.signUp({ username, password, attributes: { email, phone_number } })
-      console.log('successfully signed up!')
-      this.setState({ step: 1 })
-    } catch (err) { console.log('error signing up: ', err) }
-  }
-  confirmSignUp = async () => {
-    const { username, authenticationCode } = this.state
-    try {
-      await Auth.confirmSignUp(username, authenticationCode)
-      console.log('user successfully signed up!')
-    } catch (err) { console.log('error confirming sign up: ', err) }
+  state = { todos: [] }
+  async componentDidMount() {
+    const todoData = await API.graphql(graphqlOperation(ListTodos))
+    this.setState({ todos: todoData.data.listTodos.items })
   }
   render() {
     return (
       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
         {
-          this.state.step === 0 && (
+          this.state.todos.map((todo, i) => (
             <div>
-              <input placeholder='username' onChange={this.onChange} name='username' style={styles.input} />
-              <input placeholder='password' onChange={this.onChange} name='password' type='password' style={styles.input} />
-              <input placeholder='email' onChange={this.onChange} name='email' style={styles.input} />
-              <input placeholder='phone number' onChange={this.onChange} name='phone_number' style={styles.input} />
-              <button onClick={this.signUp}>Sign Up</button>
+              <h3>{todo.name}</h3>
+              <p>{todo.description}</p>
             </div>
-          )
-        }
-        {
-          this.state.step === 1 && (
-            <div>
-              <input placeholder='username' onChange={this.onChange} name='username' style={styles.input} />
-              <input placeholder='authentication code' onChange={this.onChange} name='authenticationCode' style={styles.input} />
-              <button onClick={this.confirmSignUp}>Confirm Sign Up</button>
-            </div>
-          )
+          ))
         }
       </div>
     );
   }
 }
 
-const styles = {
-  input: {
-    height: 35, 
-    margin: 5
-  }
-}
-
-export default App
+export default withAuthenticator(App, { includeGreetings: true })
